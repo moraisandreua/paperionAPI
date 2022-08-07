e98fe47f9bdb423ab72a634228be80c1bab8074f
André Morais <48691769+moraisandreua@users.noreply.github.com>
2022-08-07 19:41:18 +0100

    Feature Breaking upgrade
    - suppports sicnoticias.pt

---
+++ SicNoticias.py
@@ -0,0 +1,37 @@
+import requests
+from bs4 import BeautifulSoup
+
+class SicNoticias():
+    def __init__(self, response) -> None:
+        self.response=response
+
+    def fromBreaking(self):
+        retorno=[]
+        cards = self.response.find("ul", class_="js-list-articles").find_all("li")
+        c=0
+        for newsCard in cards:
+            c+=1
+            try:
+                link=newsCard.find("article").find("figure").find("a").get("href")
+                link="https://sicnoticias.pt/"+link if "https://" not in link else link
+
+                noticia={
+                    "link":link,
+                    "image":newsCard.find("article").find("figure").find("a").find("picture").find("img").get("src"),
+                    "title":newsCard.find("article").find("div").find("h2").find("a").get_text(),
+                    "text":self.getTextFromTitle(link)
+                }
+                retorno.append(noticia)
+            except:
+                continue
+        
+        return retorno
+
+    def fromSection(self, section):
+        pass
+
+    def getTextFromTitle(self, url):
+        req = requests.get(url)
+        soup = BeautifulSoup(req.content, "html.parser")
+
+        return soup.find("div", class_="item-1 item-1-1 item-odd item-first item-last first CT-html").find("p").find("strong").get_text()
--- breaking.py
+++ breaking.py
@@ -2,9 +2,11 @@ import requests
 import json
 from bs4 import BeautifulSoup
 from NoticiasAoMinuto import NoticiasAoMinuto
+from SicNoticias import SicNoticias
 
 urls_breaking=[
-    'https://www.noticiasaominuto.com/'
+    #'https://www.noticiasaominuto.com/',
+    'https://sicnoticias.pt/ultimas'
 ]
 
 content_breaking=[]
@@ -16,6 +18,9 @@ for site in urls_breaking:
     parser=None
     if "noticiasaominuto.com" in site:
         parser=NoticiasAoMinuto(soup)
+
+    if "sicnoticias.pt" in site:
+        parser=SicNoticias(soup)
     
     if parser!=None:
         content_breaking=parser.fromBreaking()
